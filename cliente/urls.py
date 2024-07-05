fdfddb81632583b56bf6d5ec00e889d162fa00e5
joaocastro <joaocastro0429@gmail.com>
2024-07-05 07:09:39 -0300

    add list django

--- cliente/urls.py
+++ cliente/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import clienteCreateView
+from .views import clienteCreateView,clienteListView
 
 urlpatterns = [
-    path("form_cliente",clienteCreateView.as_view() )
+    path("form_cliente",clienteCreateView.as_view() ),
+    path("lista_clientes",clienteListView.as_view(), name="lista_clientes"),
+    
+
 ]
--- cliente/views.py
+++ cliente/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 # from django.http  import HttpResponse
-from django.views.generic import CreateView
+from django.views.generic import CreateView , ListView,UpdateView
 from .models import Cliente
 # Create your views here.
 class clienteCreateView(CreateView):
     model=Cliente
     fields='__all__'
     template_name="form_clientes.html"
+    success_url="lista_clientes"
 
 
+class clienteListView(ListView):
+    model=Cliente
+    template_name="list_clientes.html"
+
+class clienteListUpdateView(UpdateView):
+    model=Cliente
+    fields=("nome","profissao","data_nascimento")
+    template_name="form_clientes.html"
