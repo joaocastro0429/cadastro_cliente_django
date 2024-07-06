e079d77efa7f7e6196943c72a7779a4e439ae387
joaocastro <joaocastro0429@gmail.com>
2024-07-06 16:38:51 -0300

    add delete

--- cliente/urls.py
+++ cliente/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import clienteCreateView,clienteListView,clienteListUpdateView,clienteDetailView
+from .views import clienteCreateView,clienteListView,clienteListUpdateView,clienteDetailView,clienteDeleteView
 
 urlpatterns = [
     path("form_cliente",clienteCreateView.as_view() ),
     path("lista_clientes",clienteListView.as_view(), name="lista_clientes"),
-    path("form_cliente/<int:pk>",clienteListUpdateView.as_view()),
-    path("lista_cliente/<int:pk>",clienteDetailView.as_view(),name="lista_cliente")
+    path("form_cliente/<int:pk>",clienteListUpdateView.as_view(), name="editar_cliente"),
+    path("lista_cliente/<int:pk>",clienteDetailView.as_view(),name="lista_cliente"),
+    path("remover_cliente/<int:pk>",clienteDeleteView.as_view() , name="remover_cliente")
 
 ]
--- cliente/views.py
+++ cliente/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 # from django.http  import HttpResponse
 from django.urls import reverse_lazy
-from django.views.generic import CreateView , ListView,UpdateView,DetailView
+from django.views.generic import CreateView , ListView,UpdateView,DetailView,DeleteView
 from .models import Cliente
 # Create your views here.
 class clienteCreateView(CreateView):
@@ -29,3 +29,10 @@ class clienteListUpdateView(UpdateView):
     template_name="form_clientes.html"
     # direcionando para a listagem 
     success_url=reverse_lazy("lista_clientes")
+
+
+class clienteDeleteView(DeleteView):
+    model=Cliente
+    template_name="remover_clientes.html"
+    success_url=reverse_lazy("lista_clientes")
+    
